72ede28168a50b68fde5de7289f70c5501c85a25
Kisik1709 <themegadimasssik1@gmail.com>
2025-09-12 13:49:04 +0700

    setup DialogFlow

--- support_bot.py
+++ support_bot.py
@@ -1,15 +1,35 @@
 import os
 import sys
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from dotenv import load_dotenv
+from google.cloud import dialogflow
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 
-def start(update, context):
-    update.message.reply_text("Здраствуйте!")
+def start_command(update, context):
+    update.message.reply_text("Я бот!")
 
 
-def echo(update, context):
-    update.message.reply_text(update.message.text)
+def get_dialogflow_response(project_id, session_id, text, lang_code):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+
+    text_input = dialogflow.TextInput(text=text, language_code=lang_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input})
+
+    return response.query_result.fulfillment_text
+
+
+def handle_message(update, context):
+    project_id = context.bot_data["project_id"]
+    lang_code = context.bot_data["lang_code"]
+    user_text = update.message.text
+    session_id = update.effective_user.id
+    response_text = get_dialogflow_response(
+        project_id, session_id, user_text, lang_code)
+    update.message.reply_text(response_text)
 
 
 def main():
@@ -19,12 +39,21 @@ def main():
     if not tg_token:
         sys.exit("Нет ключа доступа к TG. Завершение!")
 
+    project_id = os.getenv("PROJECT_DIALOGFLOW_ID")
+    if not project_id:
+        sys.exit("Нет id dialogflow")
+
+    lang_code = "ru"
+
     updater = Updater(token=tg_token)
     dispatcher = updater.dispatcher
 
-    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.bot_data["project_id"] = project_id
+    dispatcher.bot_data["lang_code"] = lang_code
+
+    dispatcher.add_handler(CommandHandler("start", start_command))
     dispatcher.add_handler(MessageHandler(
-        Filters.text & ~Filters.command, echo))
+        Filters.text & ~Filters.command, handle_message))
 
     updater.start_polling()
     updater.idle()
